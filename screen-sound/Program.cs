00c484a764eeab1871715a5beaa35843f0f9f37e
devfelipecesar <devfelipecesar@gmail.com>
2025-01-29 10:59:00 -0300

    add menu with switch case

--- screen-sound/Program.cs
+++ screen-sound/Program.cs
@@ -1,1 +1,55 @@
 ﻿
+//Screen Sound
+
+string mensagemDeBoasVindas = "Boas vindas ao Screen Sound\n";
+
+
+
+void ExibirMensagemDeBoasVindas()
+{
+    Console.WriteLine(@"
+░██████╗░█████╗░██████╗░███████╗███████╗███╗░░██╗  ░██████╗░█████╗░██╗░░░██╗███╗░░██╗██████╗░
+██╔════╝██╔══██╗██╔══██╗██╔════╝██╔════╝████╗░██║  ██╔════╝██╔══██╗██║░░░██║████╗░██║██╔══██╗
+╚█████╗░██║░░╚═╝██████╔╝█████╗░░█████╗░░██╔██╗██║  ╚█████╗░██║░░██║██║░░░██║██╔██╗██║██║░░██║
+░╚═══██╗██║░░██╗██╔══██╗██╔══╝░░██╔══╝░░██║╚████║  ░╚═══██╗██║░░██║██║░░░██║██║╚████║██║░░██║
+██████╔╝╚█████╔╝██║░░██║███████╗███████╗██║░╚███║  ██████╔╝╚█████╔╝╚██████╔╝██║░╚███║██████╔╝
+╚═════╝░░╚════╝░╚═╝░░╚═╝╚══════╝╚══════╝╚═╝░░╚══╝  ╚═════╝░░╚════╝░░╚═════╝░╚═╝░░╚══╝╚═════╝░
+");
+    Console.WriteLine(mensagemDeBoasVindas);
+}
+
+
+void ExibirOpcoesDoMenu()
+{
+    Console.WriteLine("Digite 1 para registra uma banda");
+    Console.WriteLine("Digite 2 para mostrar todas as bandas");
+    Console.WriteLine("Digite 3 para avaliar uma banda");
+    Console.WriteLine("Digite 4 para exibir a média de uma banda");
+    Console.WriteLine("Digite -1 para sair");
+
+    Console.Write("\nDigite a sua opção: ");
+    string opcaoEscolhida = Console.ReadLine()!;
+    int opcaoEscolhidaNumerica = int.Parse(opcaoEscolhida);
+
+    switch (opcaoEscolhidaNumerica)
+    {
+        case 1: Console.WriteLine("Voce escolheu a opcao: "+ opcaoEscolhidaNumerica);
+            break;
+        case 2: Console.WriteLine("Voce escolhey a opcao: " + opcaoEscolhidaNumerica);
+            break;
+        case 3:
+            Console.WriteLine("Voce escolhey a opcao: " + opcaoEscolhidaNumerica);
+            break;
+        case 4:
+            Console.WriteLine("Voce escolhey a opcao: " + opcaoEscolhidaNumerica);
+            break;
+        case -1:
+            Console.WriteLine("Tchau, tchau :)");
+            break;
+        default: Console.WriteLine("Opcao invalida!");
+            break;
+    }
+}
+
+ExibirMensagemDeBoasVindas();
+ExibirOpcoesDoMenu();
